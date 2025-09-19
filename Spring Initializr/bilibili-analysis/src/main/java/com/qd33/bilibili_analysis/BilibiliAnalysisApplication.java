7107719fdda7afa5302799ff8ea7767295d853c0
qd33 <2960691238@qq.com>
2025-09-19 17:59:07 +0800

    Delete bilibili_demo.py

---
+++ Spring Initializr/bilibili-analysis/src/main/java/com/qd33/bilibili_analysis/BilibiliAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.qd33.bilibili_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilibiliAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilibiliAnalysisApplication.class, args);
+	}
+
+}
---
+++ Spring Initializr/bilibili-analysis/src/test/java/com/qd33/bilibili_analysis/BilibiliAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qd33.bilibili_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilibiliAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
